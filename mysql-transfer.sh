39190e14ae9f32a1f4d4b0f69a11f37263a50c3b
Sinan <43215895+SinanAkkoyun@users.noreply.github.com>
2021-11-11 15:21:25 +0100

    Update mysql-transfer.sh

--- mysql-transfer.sh
+++ mysql-transfer.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+while getopts ":ht" opt; do
+  case ${opt} in
+    u ) 
+      u="$OPTARG"
+      ;;
+    s ) # process option t
+      s="$OPTARG"
+      ;;
+    d ) # process option t
+      d="$OPTARG"
+      ;;
+    n ) # process option t
+      n="$OPTARG"
+      ;;
+    p ) # process option t
+      p="$OPTARG"
+      ;;
+    
+    \? ) echo "Usage: mysql-transfer [-u username] [-p password] [-n db name] [-s source host] [-d destination host]"
+      ;;
+  esac
+done
+
+mysqldump -h $s --verbose -u $u --password=$p $n | mysql -u $u --password=$p -h $d --password=$p $n
